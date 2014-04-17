b6187e979c519798e86e48b6a3aeeae7e94872e4
jgrindall <john.grindall@gmail.com>
2014-04-18 01:42:01 +0530

    to check the number of files open

--- public/js/views/help/helpoverlay.js
+++ public/js/views/help/helpoverlay.js
@@ -1,7 +1,12 @@
 LG.HelpOverlayView = Backbone.View.extend({
 	template:"tpl_helpoverlay",
 	initialize:function(){
-		
+		var _this = this;
+		setInterval(function(){
+			if(LG.fileCollection){
+				_this.$("p").text("Help!  "+LG.fileCollection.length);
+			}
+		}, 1000);
 	},
 	events:function(){
 		var obj = Backbone.View.getTouch( {	} );
