e8274284939bccca4789a5106733ec779f713d33
jgrindall <john.grindall@gmail.com>
2014-04-21 17:23:54 +0530

    added buttons to overlay

--- public/js/views/help/helpoverlay.js
+++ public/js/views/help/helpoverlay.js
@@ -1,16 +1,48 @@
 LG.HelpOverlayView = Backbone.View.extend({
 	template:"tpl_helpoverlay",
 	initialize:function(){
-		var _this = this;
-		setInterval(function(){
-			if(LG.fileCollection){
-				_this.$("p").text("Help!  "+LG.fileCollection.length);
-			}
-		}, 1000);
+		this.page = 0;
 	},
 	events:function(){
-		var obj = Backbone.View.getTouch( {	} );
+		var obj = Backbone.View.getTouch( {
+			"_click button.next":"clickNext",
+			"_click button.copy":"clickCopy",
+			"_click button.more":"clickMore",
+			"_click button.draw":"clickDraw",
+			"_click":"clickMe"
+		} );
 		return obj;
+	},
+	clickDraw:function(e){
+		this.stopProp(e);
+		LG.EventDispatcher.trigger(LG.Events.CLICK_DRAW_START);
+	},
+	clickNext:function(e){
+		this.stopProp(e);
+		this.next();
+	},
+	clickMore:function(e){
+		this.stopProp(e);
+		LG.EventDispatcher.trigger(LG.Events.HIDE_HELP_OVERLAY);
+		LG.router.navigate("help", {"trigger":true});
+	},
+	clickCopy:function(e){
+		this.stopProp(e);
+		this.$("button.draw").css("display", "block");
+		this.copy();
+	},
+	copy:function(){
+		var s = "fd(100);rt(90);fd(100);rt(90);fd(100);rt(90);fd(100);rt(90);";
+		LG.fileCollection.selected.set({"logo":s});
+	},
+	next:function(){
+		this.$el.removeClass("help"+this.page);
+		this.page = (this.page + 1) % LG.HelpOverlayView.NUM_PAGES;
+		this.$el.addClass("help"+this.page);
+	},
+	clickMe:function(e){
+		this.stopProp(e);
+		LG.EventDispatcher.trigger(LG.Events.HIDE_HELP_OVERLAY);
 	},
 	render:function(){
 		this.loadTemplate(  this.template, {},  {replace:true}  );
@@ -20,4 +52,6 @@ LG.HelpOverlayView = Backbone.View.extend({
 	
 	}
 });
+
+LG.HelpOverlayView.NUM_PAGES = 4;
 
