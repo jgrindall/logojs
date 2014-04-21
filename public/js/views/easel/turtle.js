a7337de6afa85ecf8526f5acb6273d8c993bb80b
jgrindall <john.grindall@gmail.com>
2014-04-21 17:22:46 +0530

    new non-hardcoded colour

--- public/js/views/easel/turtle.js
+++ public/js/views/easel/turtle.js
@@ -2,8 +2,9 @@
 window.LG.Easel = window.LG.Easel || {};
 
 LG.Easel.Turtle = function(size) {
-  this.initialize(size);
+	this.initialize(size);
 }
+
 LG.Easel.Turtle.prototype = new createjs.Shape();
 
 LG.Easel.Turtle.prototype.initialize = function(size) {
@@ -11,8 +12,8 @@ LG.Easel.Turtle.prototype.initialize = function(size) {
 	this.size = size;
 	var g = this.graphics;
 	g.setStrokeStyle(0);
-	g.beginStroke(LG.color);
-	g.beginFill(LG.color);
+	g.beginStroke(LG.graphicsModel.get("color"));
+	g.beginFill(LG.graphicsModel.get("color"));
 	g.moveTo(-this.size,-this.size);
 	g.lineTo(this.size,0);
 	g.lineTo(-this.size,this.size);
