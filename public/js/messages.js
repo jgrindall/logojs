1f72dab3c6c79e78adaab05e5e4c042bbc1b651e
jgrindall <john.grindall@gmail.com>
2014-05-01 20:37:53 +0530

    new text for www errors

--- public/js/messages.js
+++ public/js/messages.js
@@ -2,4 +2,5 @@
 LG.Messages = {};
 LG.Messages.WANT_TO_SAVE = "Do you want to save your current file?";
 LG.Messages.WANT_TO_DELETE = "Are you sure you want to delete this file?";
-
+LG.Messages.ERROR = "Error!";
+LG.Messages.ERROR_BODY = "An error has occured connecting to the internet. You cannot save or load files without an internet connection.";
