d28125857cd08b6ffb876035d3d2bc81a7d2f389
jgrindall <john.grindall@gmail.com>
2014-05-01 20:38:55 +0530

    so that we can open from anywhere

--- public/js/models/fileopener.js
+++ public/js/models/fileopener.js
@@ -1,7 +1,7 @@
 
 
 LG.FileOpener = function(){
-
+	_.extend(this, Backbone.Events);
 };
 
 LG.FileOpener.prototype.open = function(id){
@@ -21,13 +21,53 @@ LG.FileOpener.prototype.open = function(id){
 	else{
 		userId = LG.userModel.get("userId");
 		if(oldModelYours && oldModelYours.get("userId") === userId){
-			// yours!
+			console.log("yours");
 			LG.fileCollection.loadById(id);
 		}
 		else{
+			console.log("not yours");
 			LG.fileCollection.openOthers(oldModel.toJSON());
 		}
 	}
 };
 
+LG.FileOpener.prototype.openFile = function(){
+	LG.router.navigate("write/"+this.id, {"trigger":true});
+};
 
+LG.FileOpener.prototype.alertOk = function(){
+	LG.fileCollection.save();
+};
+
+LG.FileOpener.prototype.alertNo = function(){
+	this.stopListening(LG.fileCollection, "sync");
+	this.openFile();
+};
+
+LG.FileOpener.prototype.alertCancel = function(){
+	this.stopListening(LG.fileCollection, "sync");
+	this.openFile();
+};
+
+LG.FileOpener.prototype.modelSynced = function(){
+	this.stopListening(LG.fileCollection, "sync");
+	this.openFile();
+};
+
+LG.FileOpener.prototype.openFromGallery = function(id){
+	var options;
+	this.id = id;
+	if(LG.userModel.isConnected()){
+		if(!LG.fileCollection.selected.isSaved()){
+			options = {"ok":$.proxy(this.alertOk, this), "no":$.proxy(this.alertNo, this), "cancel":$.proxy(this.alertCancel, this) };
+			LG.popups.openPopup({"message":LG.Messages.WANT_TO_SAVE,  "okColor":1, "noColor":2, "okLabel":"Yes", "noLabel":"No"}, options);
+			this.listenTo(LG.fileCollection, "sync", $.proxy(this.modelSynced, this));
+		}
+		else{
+			this.openFile();
+		}
+	}
+	else{
+		this.openFile();
+	}
+};
