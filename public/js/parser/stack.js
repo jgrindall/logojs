206819dc5bfea5b32d06b28391a9c2cbda9ddf4a
jgrindall <john.grindall@gmail.com>
2014-05-02 19:01:11 +0530

    renamed

--- public/js/parser/stack.js
+++ public/js/parser/stack.js
@@ -1,14 +1,14 @@
 
-LG.stack = function(){
+LG.Stack = function(){
 	this.s = [];
 };
 
 
-LG.stack.prototype.push = function(n){
+LG.Stack.prototype.push = function(n){
 	this.s.push(n);
 };
 
-LG.stack.prototype.pop = function(){
+LG.Stack.prototype.pop = function(){
 	var r = this.s.pop();
 	return r;
 };
