eb3a91f2e26801126183b5b8a475cf974dc4efa9
John <john.grindall@gmail.com>
2014-05-14 23:14:10 +0530

    added examples

---
+++ public/js/app/views/help/examples.js
@@ -0,0 +1,60 @@
+
+LG.ExamplesView = LG.AMenuView.extend({
+	template:"tpl_examples",
+	initialize:function(){
+		LG.AMenuView.prototype.initialize.call(this);
+	},
+	events:function(){
+		var obj = Backbone.View.getTouch( {
+			"_click button.next":"clickNext",
+			"_click button.copy":"clickCopy",
+			"_click button.more":"clickMore",
+			"_click button.draw":"clickDraw",
+			"_click .close":"clickClose",
+			"_click":"clickMe"
+		} );
+		return obj;
+	},
+	showName:"examples",
+	clickDraw:function(e){
+		this.stopProp(e);
+		LG.EventDispatcher.trigger(LG.Events.CLICK_DRAW_START);
+	},
+	clickNext:function(e){
+		this.stopProp(e);
+	},
+	clickMore:function(e){
+		this.stopProp(e);
+		LG.router.navigate("help", {"trigger":true});
+	},
+	clickCopy:function(e){
+		this.stopProp(e);
+	},
+	clickClose:function(){
+		window.history.back();
+	},
+	copy:function(){
+		var s = "rpt 6[\nfd(100);rt(60);\n]";
+		LG.EventDispatcher.trigger(LG.Events.FORCE_LOGO, s);
+	},
+	clickMe:function(e){
+		this.stopProp(e);
+		LG.EventDispatcher.trigger(LG.Events.HIDE_HELP_OVERLAY);
+	},
+	onShow:function(){
+	
+	},
+	onHide:function(){
+		
+	},
+	render:function(){
+		this.loadTemplate(  this.template, {},  {replace:true}  );
+		return this;
+	},
+	beforeClose:function(){
+	
+	}
+});
+
+
+
