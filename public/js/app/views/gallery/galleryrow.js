13580ee0f287998012315f205f399f9eefb8f7e5
John <john.grindall@gmail.com>
2014-07-16 21:08:22 +0100

    tweaks

--- public/js/app/views/gallery/galleryrow.js
+++ public/js/app/views/gallery/galleryrow.js
@@ -19,6 +19,9 @@ LG.AGalleryRowView = Backbone.View.extend({
 	},
 	render:function(){
 		var data = this.model.toJSON();
+		var logo = data.logo;
+		data.logo1 = logo.split('\n')[0];
+		data.logo2 = logo.split('\n')[1];
 		this.loadTemplate(  this.template, data , {replace:true} );
 		this.updateLayout();
 		return this;
