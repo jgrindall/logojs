6e44d004541cd950ddda4692e28a9d5cdf0d6733
John <john.grindall@gmail.com>
2014-07-17 13:38:52 +0100

    video changes

--- public/js/app/views/help/examples.js
+++ public/js/app/views/help/examples.js
@@ -59,7 +59,7 @@ LG.ExamplesView.LOGO = [ ];
 LG.ExamplesView.LOGO.push("fd(100) rt(90)\nfd(100) rt(90)\nfd(100) rt(90)\nfd(100) rt(90)");
 LG.ExamplesView.LOGO.push("fd(50) rt(45)\npenup() fd(50) rt(45) pendown()\nfd(50) rt(45)\npenup() fd(50) rt(45) pendown()\nfd(50) rt(45)\npenup() fd(50) rt(45) pendown()\nfd(50) rt(45) penup() fd(50) rt(45) pendown()");
 LG.ExamplesView.LOGO.push("bg(gray)\nthick(4) color(yellow) fd(30)\nthick(6) color(blue) fd(30)\nthick(8) color(orange) fd(30)\nthick(10) color(red) fd(30)");
-LG.ExamplesView.LOGO.push("bg(orange)\ncolor(white)\nn:=16\ns=200\nrpt n\n    fd(s) rt(180 - 360/n)\nendrpt");
+LG.ExamplesView.LOGO.push("bg(orange)\ncolor(white)\nn=16\ns=200\nrpt n\n    fd(s) rt(180 - 360/n)\nendrpt");
 LG.ExamplesView.LOGO.push("bg(blue)\ncolor(yellow)\nthick(10)\nn=4\nproc drawsquare\n    rpt n\n        fd(100) rt(90)\n    endrpt\nendproc\nrpt 8\n    drawsquare()\n    rt(45)\nendrpt");
 LG.ExamplesView.LOGO.push("a=5\nproc drawpoly(side, n)\n    rpt n\n        fd(side) rt(360/n)\n    endrpt\nendproc\nrpt 10\n    drawpoly(25,a)\n    a=a+4\nendrpt\n");
 LG.ExamplesView.LOGO.push("bg(blue) color(white)\nrpt 90 fd(1)rt(1) endrpt\nrt(270)\nrpt 180 fd(1)rt(1) endrpt\nrt(270)\nrpt 90 fd(1)rt(1) endrpt");
