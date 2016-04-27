381c49167c145d8e2282c23960a76809944f834e
John <john.grindall@gmail.com>
2016-04-27 21:19:17 +0100

    tidy

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-var express, app, port, exec, fs, mkdirp, mongoose, nodemailer, smtpTransport;
-var File, FileSchema, saveImage, mongoUri, maxFiles, minFiles, activate, AWS, s3Explorer;
+var express, app, port, bodyParser;
 
 express = require("express");
 app = express();
 port = Number(process.env.PORT || 5000);
-var bodyParser = require('body-parser');
-app.use(express.static(__dirname+"/public"));
+bodyParser = require('body-parser');
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser());
+
 app.get('/', function(req, res){
 	res.render("index.jade");
 });
